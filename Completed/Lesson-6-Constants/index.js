595c9149a5304fa53e0e0ba0d58c585724e5a4a1
MMStacked <102634576+MMStacked@users.noreply.github.com>
2025-07-09 18:50:48 +0100

    Add files via upload

---
+++ Completed/Lesson-6-Constants/index.js
@@ -0,0 +1,18 @@
+// Const = a variable that can't be changed
+
+const PI = 3.14159;
+let radius;
+let circumference;
+
+//    Window prompt
+// radius = window.prompt(`Enter the radius of the circle: `);
+//radius = Number(radius);
+
+
+
+document.getElementById("mySubmit").onclick = function(){
+  radius = document.getElementById("myText").value;
+  radius = Number(radius);
+  circumference = 2 * PI * radius;
+  document.getElementById("myH3").textContent = circumference + "cm";
+}
