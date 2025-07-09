9e917ac680a14b816550bd479715f576bffd84da
MMStacked <102634576+MMStacked@users.noreply.github.com>
2025-07-09 14:14:57 +0100

    Add files via upload

---
+++ Completed/Lesson-1-JavaScript-Basics/index.js
@@ -0,0 +1,17 @@
+console.log(`Hello!`);
+console.log(`I like pizz`);
+
+// window.alert(`This is an alert!`);
+// Window.alert(`I like pizza`);
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById(`myP`).textContent = `I like pizza!`;
+
+
+// This is a comment
+
+/* this
+   is
+   a 
+   comment
+*/
