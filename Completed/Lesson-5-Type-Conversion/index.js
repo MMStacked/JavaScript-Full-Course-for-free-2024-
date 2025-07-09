e7a8a2b7d1aaa8c551ffb5f20645b081639144f9
MMStacked <102634576+MMStacked@users.noreply.github.com>
2025-07-09 18:14:31 +0100

    Add files via upload

---
+++ Completed/Lesson-5-Type-Conversion/index.js
@@ -0,0 +1,32 @@
+// type conversion = change the datatype of a value to another
+//                   (strings, numbers, booleans)
+
+// Convert to Int  = age = Number(age);
+
+/*  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+let age = window.prompt("How old are you?");
+//    Converts the type from string (user input) into a number
+age = Number(age);
+
+
+age+=1;
+
+console.log(age, typeof age);
+*/
+
+//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+let x = "";
+let y = "";
+let z = "";
+
+
+//    convert variable into
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
